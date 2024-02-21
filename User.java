8eb48e973f2fbe4cbc44abf30c88d2248baa021d
Davi Coelho <davicoelho.dev@gmail.com>
2024-02-20 22:06:39 -0300

    Added getters and setters methods to the User class

--- User.java
+++ User.java
@@ -1,12 +1,28 @@
 //This class will represent the user to the system
 class User {
-    String username;
-    String email;
-    String password;
+    public String username;
+    private String email;
+    private String password;
 
     User(String username, String email, String password) {
         this.username = username;
         this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
