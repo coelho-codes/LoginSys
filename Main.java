68b34f8a1f88d5b4e4516c258ecb9b3a00612f33
Davi Coelho <davicoelho.dev@gmail.com>
2024-02-20 22:20:35 -0300

    Started to create basic structure of code in main method(variables, methods and infinite for loop)

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws java.io.IOException {
+        Main ob = new Main();
         Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("Are you already registered in our system(y or n)?\n>>>");
+
+        for(;;) {
+            
+        }
+    }
+
+    void signUp() {
+
+    }
+
+    void logIn() {
+        
     }
 }
